42153a5b91bd7812191bd554b16352848ccc3214
Nazgugu <prada520farrell@gmail.com>
2015-03-08 22:58:00 -0700

    revert

---
+++ healthCare/healthCare/Controllers/DoctorViewController.h
@@ -0,0 +1,13 @@
+//
+//  DoctorViewController.h
+//  healthCare
+//
+//  Created by Liu Zhe on 3/8/15.
+//  Copyright (c) 2015 Liu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoctorViewController : UIViewController
+
+@end
