82e83bad23867b090c3a73fcfb1f44809394c746
Nazgugu <prada520farrell@gmail.com>
2015-03-08 22:31:58 -0700

    initialize project

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  healCareSystem
+//
+//  Created by Liu Zhe on 3/8/15.
+//  Copyright (c) 2015 Liu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
